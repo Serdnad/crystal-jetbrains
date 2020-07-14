0d6bca8b90b92408847d0747ea5343872a9ab0d0
GitHub Action <action@github.com>
2020-07-14 10:21:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/serdnad/crystaljetbrains/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/serdnad/crystaljetbrains/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.serdnad.crystaljetbrains.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.serdnad.crystaljetbrains.MyBundle
 
 class MyApplicationService {
 
